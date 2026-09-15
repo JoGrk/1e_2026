b21b4c3473af83797ecd604bc2145dd671b6b811
Jolanta Gryko <jgryko@gmail.com>
2026-09-15 13:20:45 +0200

    15.09

---
+++ 01_podstawy/01_print.py
@@ -0,0 +1,10 @@
+# print("hello world")
+# a = int(input("podaj liczbę"))
+# print(a*a)
+
+# przeliczanie stopni fahrenheita na celsiusza
+
+celsiusz = int(input("Podaj stopnie Celsiusza: "))
+fahrenheit = (celsiusz * 9/5 + 32)
+print("stopnie fahrenheit wynoszą", "tekst")
+
